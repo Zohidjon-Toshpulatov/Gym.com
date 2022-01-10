7a5a538b77f54acc87d58b2e1242d4c364a325a5
Zohidjon-Toshpulatov <zohid99jon@gmail.com>
2022-01-10 19:47:57 +0500

    Project created

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,css}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        'body': '#F8FAFC',
+        'btn-blue': '#3679F6',
+        'link-hover': '#2056AE',
+        'boder-color': '#E2E8F0',
+      },
+    },
+  },
+  plugins: [],
+}
